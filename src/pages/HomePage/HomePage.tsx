a6d024781de8ee17d7b254f37fdb6a02634b32c1
Aliaksei Domut <alexedomut@gmail.com>
2024-11-08 21:43:08 +0300

    feat: add search input

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./HomePage.module.css";
+import Search from "../../components/Search/Search";
+import useDebounce from "../../utils/hooks/useDebounce";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const [searchValue, setSearchValue] = useState("");
+
+  const debouncedSearchValue = useDebounce(searchValue);
+
+  useEffect(() => {
+    if (debouncedSearchValue) {
+      console.log(debouncedSearchValue);
+    }
+  }, [debouncedSearchValue]);
+
+  const handleSearch = (str: string) => {
+    setSearchValue(str);
+  };
+
+  return (
+    <div className={styles.HomePage}>
+      <h2 className={styles.h2}>
+        Let&apos;s Find Some <span style={{ color: "#F17900" }}>Art</span> Here!
+      </h2>
+
+      <Search onSearch={handleSearch} />
+    </div>
+  );
 }
