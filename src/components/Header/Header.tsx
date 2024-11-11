13ba3053dcca07afdc5401e41c8b51840188454c
Aliaksei Domut <alexedomut@gmail.com>
2024-11-12 02:29:06 +0300

    style: add adaptive to footer and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,36 +5,42 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.Header}>
-      <NavLink to="/">
-        <img
-          className={styles.logo}
-          src="https://raw.githubusercontent.com/aliakseidomut/modsen-art-museum4-main/8a35c43bba1f5065c852af2b5d0fbd4925472ba6/src/assets/images/museum-logo.svg"
-          alt="museum logo"
-        />
-      </NavLink>
-      <nav className={styles.nav}>
-        <NavLink
-          className={({ isActive }) => (isActive ? styles.hidden : styles.link)}
-          to="/"
-        >
+      <div className={styles.container}>
+        <NavLink to="/">
           <img
-            src="https://raw.githubusercontent.com/aliakseidomut/modsen-art-museum4-main/8a35c43bba1f5065c852af2b5d0fbd4925472ba6/src/assets/images/home.svg"
-            alt="home icon"
+            className={styles.logo}
+            src="https://raw.githubusercontent.com/aliakseidomut/modsen-art-museum4-main/8a35c43bba1f5065c852af2b5d0fbd4925472ba6/src/assets/images/museum-logo.svg"
+            alt="museum logo"
           />
-          <span>Home</span>
         </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? styles.hidden : styles.link)}
-          to="/favorites"
-        >
-          <img
-            src="https://raw.githubusercontent.com/aliakseidomut/modsen-art-museum4-main/8a35c43bba1f5065c852af2b5d0fbd4925472ba6/src/assets/images/bookmark.svg"
-            alt="favorites icon"
-            height={23}
-          />
-          <span>Your favorites</span>
-        </NavLink>
-      </nav>
+        <nav className={styles.nav}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.hidden : styles.link
+            }
+            to="/"
+          >
+            <img
+              src="https://raw.githubusercontent.com/aliakseidomut/modsen-art-museum4-main/8a35c43bba1f5065c852af2b5d0fbd4925472ba6/src/assets/images/home.svg"
+              alt="home icon"
+            />
+            <span>Home</span>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.hidden : styles.link
+            }
+            to="/favorites"
+          >
+            <img
+              src="https://raw.githubusercontent.com/aliakseidomut/modsen-art-museum4-main/8a35c43bba1f5065c852af2b5d0fbd4925472ba6/src/assets/images/bookmark.svg"
+              alt="favorites icon"
+              height={23}
+            />
+            <span>Your favorites</span>
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
